e1637fc9fd4f058d40ad9f0c1125b9676e72621e
Tyler Hancock <darklime@live.ca>
2022-06-15 02:25:14 -0600

    Allow any client to connect to servers with this mod installed.

--- Forge/src/main/java/net/darkhax/whatthebucket/WhatTheBucketForge.java
+++ Forge/src/main/java/net/darkhax/whatthebucket/WhatTheBucketForge.java
@@ -1,13 +1,17 @@
 package net.darkhax.whatthebucket;
 
 import net.darkhax.whatthebucket.impl.fixes.MC151457;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Constants.MOD_ID)
 public class WhatTheBucketForge {
 
     public WhatTheBucketForge() {
 
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         MC151457.applyFix();
     }
 }
